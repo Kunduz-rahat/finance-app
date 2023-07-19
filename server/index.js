bd21fb0771defaec191bc2afe6a50cbe31fc9b87
Kunduz Rahat <rahatova.kunduz@gmail.com>
2023-07-19 12:10:13 +0600

    delete node-modules

--- server/index.js
+++ server/index.js
@@ -1,1 +1,30 @@
-import { loadType } from "mongoose-currency";
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import morgan from "morgan";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+const URL = process.env.MONGO_URL;
+
+mongoose
+  .connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    app.listen(PORT, () => console.log(`server started in ${PORT}`));
+  })
+  .catch((e) => console.log(e));
